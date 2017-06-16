c4a02c98a3500c2093a51eb44da0c591bc106511
Bruce <brucelau.email@gmail.com>
2017-06-16 18:11:38 -0400

    client try to reconnect server if failure

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import socket               # Import socket module
 import sys
+import time
 
 #arg = sys.argv[1]
 #host = socket.gethostyname(args) # Get local machine name
@@ -8,7 +9,15 @@ host = 'liu1ee.cs.uwindsor.ca' # Get local machine name
 port = 5000
 
 s = socket.socket()         # Create a socket object
-s.connect((host, port))
+is_connected = False
+while not is_connected:
+    try:
+        s.connect((host, port))
+        is_connected = True
+    except socket.error, e:
+        print 'error to connect server: ', host
+        print 'retry in 5s'
+        time.sleep(2)
 
 while True:
     backstr = s.recv(1024)
