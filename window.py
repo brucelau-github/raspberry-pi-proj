a50e0d234bab6e0e8abc9751578abe1f19a08e09
Bruce <brucelau.email@gmail.com>
2017-06-23 12:02:43 -0400

    add class app, two buttons

--- window.py
+++ window.py
@@ -1,6 +1,24 @@
 from Tkinter import *
 
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.label = Label(frame, text="quit", fg="red")
+        self.label.pack(side=TOP)
+
+        self.button = Button(frame, text="quit", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
 root = Tk() #create basic window
-thelabel = Label(root, text="this is very easy") #add a latbel
-thelabel.pack()
+
+app = App(root)
 root.mainloop() #keep the windows showing
+root.destroy()
