2a01551a288c39b31ee5cffe0799e1158a8a2cfe
Bruce <brucelau.email@gmail.com>
2017-06-23 20:29:29 -0400

    add class window main class app,update label text.

--- window.py
+++ window.py
@@ -5,7 +5,9 @@ class App:
         frame = Frame(master)
         frame.pack()
 
-        self.label = Label(frame, text="quit", fg="red")
+        self.msg = StringVar()
+        self.msg.set("test")
+        self.label = Label(frame, textvariable=self.msg, fg="red")
         self.label.pack(side=TOP)
 
         self.button = Button(frame, text="quit", fg="red", command=frame.quit)
@@ -14,11 +16,16 @@ class App:
         self.hi_there = Button(frame, text="Hello", command=self.say_hi)
         self.hi_there.pack(side=LEFT)
 
+
     def say_hi(self):
+        self.msg.set("hello world")
         print "hi there, everyone!"
 
 root = Tk() #create basic window
 
 app = App(root)
+
+app.msg.set("startup")
+
 root.mainloop() #keep the windows showing
 root.destroy()
