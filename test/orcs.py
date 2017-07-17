209571f223fa9130a715cc5674df562f6e718c27
Bruce <brucelau.email@gmail.com>
2017-07-17 16:07:54 -0400

    add orcs

---
+++ test/orcs.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+import random
+
+if __name__ == '__main__':
+	keep_playing = 'y'
+	occupants = ['enemy', 'friend', 'unoccupied']
+	width = 72
+	dotted_line = '-' * width
+	print(dotted_line)
+	print("\033[1m" + "Attack of The orcs V 0.0.1:" + "\033[0m")
