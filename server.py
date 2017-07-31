8d9820562965977454a48c2aadfc479ab7d12f10
Bruce <brucelau.email@gmail.com>
2017-07-31 09:15:43 -0400

    change server into object orientated class

--- server.py
+++ server.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 import socket               # Import socket module
+class Server:
+    def __init__(self):
+        self.s = socket.socket()         # Create a socket object
+        self.host = socket.gethostname() # Get local machine name
+        self.port = 5000                 # Reserve a port for your service.
+        self.s.bind((host, port))        # Bind to the port
+        self.s.listen(5)                 # Now wait for client connection.
+        print 'server running on port {0}'.format(port)
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 5000                 # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+    def serve(self):
+        while True:
+            c, addr = s.accept()     # Establish connection with client.
+            print 'Got connection from', addr
+            while True:
+                print 'enter your message:'
+                msg = raw_input()
+                if msg == 'bye!':
+                print msg
+                c.send(msg)
+                break
+                    else:
+                    c.send(msg)
+                print 'waiting for another client'
 
-s.listen(5)                 # Now wait for client connection.
-print 'server running on port {0}'.format(port)
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   while True:
-        print 'enter your message:'
-        msg = raw_input()
-        if msg == 'bye!':
-            print msg
-            c.send(msg)
-            break
-        else:
-            c.send(msg)
-   print 'waiting for another client'
-
-   c.close()                # Close the connection
+                c.close()                # Close the connection
