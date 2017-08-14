2531c7b07454a42e2216146af5603dc709aae316
brucelau_github <brucelau.email@gmail.com>
2017-08-14 11:33:18 -0400

    change socket to block mode using select to detect if socket has message incoming.

--- connector.py
+++ connector.py
@@ -2,18 +2,24 @@
 import socket               # Import socket module
 import sys
 import threading
+import select
 
-SOCKET_TIMEOUT = 1.0
 
 class Connector:
+
     def __init__(self, host='localhost', port=5000):
         self.host = host
         self.port = port
-        self.skt = socket.socket()         # Create a socket object
-        self.skt.settimeout(SOCKET_TIMEOUT)
+        self.skt = socket.socket()
         self.is_connected = False
         self.request_shutdown = False
         self.connect()
+
+    def fileno(self):
+        """Return socket file number.
+        Interface required by select().
+        """
+        return self.skt.fileno()
 
     def connect(self):
         try:
@@ -22,21 +28,34 @@ class Connector:
         except socket.error as socketerror:
             print 'error to connect server: {0}'.format(self.host)
             raise socketerror 
+
     def getmsg(self):
-        while not self.request_shutdown:
-            try:
-                backstr = self.skt.recv(1024)
-                if backstr:
-                    print(backstr)
-            except socket.timeout:
-                print("recv timeout")
-            except socket.error:
-                self.disconnect()
-                print("socket disconnected")
-                break
+        backstr = self.skt.recv(1024)
+        if backstr:
+            return backstr
 
     def disconnect(self):
+        self.is_connected = False
         self.skt.close()
+
+    def serve_forever(self, poll_interval=0.5):
+        """Monitor message coming from the socket
+
+        Polls for shutdown every poll_interval seconds. Ignores
+        self.timeout. If you need to do periodic tasks, do them in
+        another thread.
+        """
+        try:
+            while not self.request_shutdown:
+                r, w, e = select.select([self], [], [], poll_interval)
+                if self in r:
+                    self.handle_message()
+        finally:
+            self.disconnect()
+
+    def handle_message(self, app):
+        app.update_msg(getmsg())
+
 if __name__ == '__main__':
     c = Connector()
     c.getmsg()
