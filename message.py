6f48eaaddf7937de01e09f9e6597b5ef288f3c12
brucelau_github <brucelau.email@gmail.com>
2017-08-18 17:05:34 -0400

    add tostring method on message class

--- message.py
+++ message.py
@@ -30,10 +30,25 @@ class Message:
             ret_str = ret_str + '{0}: {1}\n'.format(str(k),str(v))
         return ret_str
 
+    def get_header_as_string(self):
+        return self.get_header()
+
     def set_header(self, key, value):
         if not hasattr(self, '_header'):
             self._header = {}
         self._header[key] = value
+
+    def set_body(self, messagebody=None):
+        self.messagebody = messagebody
+        self.set_header(CONTENTLENGTH,messagebody.length())
+
+    def get_body_as_string(self):
+        if not self.messagebody:
+            return ""
+        return self.messagebody.as_string()
+
+    def as_string(self):
+        return self.get_header_as_string() + "\n" + self.get_body_as_string()
 
 
 class MessageBody:
@@ -44,6 +59,9 @@ class MessageBody:
         return len(self.body)
 
     def __str__(self):
+        return self.as_string()
+
+    def as_string(self):
         return self.body
 
 class TextMessage(Message):
